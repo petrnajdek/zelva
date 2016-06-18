4bf794212ce1bbd26868346f39fa69d2cd5e1c8a
campovski <campovski@gmail.com>
2016-06-18 17:59:10 +0000

    Rename light_switcher.py and switch pins for heat and light.

--- light_switcher.py
+++ light_switcher.py
@@ -1,27 +1,21 @@
+#!/usr/bin/python3.4
 if __name__ == '__main__':
 	import RPi.GPIO as GPIO
 	import argparse
 
 	TURN_ON = '1'
 	TURN_OFF = '0'
-	PIN_LIGHT = 32
-	PIN_HEAT = 36
+	PIN_HEAT = 32
+	PIN_LIGHT = 36
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("light")
 	parser.add_argument("heat")
+	parser.add_argument("light")
 	args = parser.parse_args()
 
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(PIN_HEAT, GPIO.OUT)
 	GPIO.setup(PIN_LIGHT, GPIO.OUT)
-
-	if args.light  == TURN_ON:
-		GPIO.output(PIN_LIGHT, 1)
-		print("Light ON")
-	elif args.light == TURN_OFF:
-		GPIO.output(PIN_LIGHT, 0)
-		print("Light OFF")
 
 	if args.heat == TURN_ON:
 		GPIO.output(PIN_HEAT, 1)
@@ -29,3 +23,10 @@ if __name__ == '__main__':
 	elif args.heat == TURN_OFF:
 		GPIO.output(PIN_HEAT, 0)
 		print("Heat OFF")
+
+	if args.light == TURN_ON:
+		GPIO.output(PIN_LIGHT, 1)
+		print("Light ON")
+	elif args.light == TURN_OFF:
+		GPIO.output(PIN_LIGHT, 0)
+		print("Light OFF")
